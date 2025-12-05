f44d428019a031892f5fe54b31eaa19060541da1
Anna Horodiuk <annahoroduk@gmail.com>
2025-12-05 23:34:42 +0200

    Fixed project structure and finished Lab 3

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,35 @@ const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
 const fileInclude = require('gulp-file-include');
 
-// HTML
+// HTML: шукає index.html в src і збирає шматочки з components
 function html() {
-  return src('src/*.html')
-    .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
+  return src('src/*.html') 
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
 
-// SCSS Task
+// SCSS: бере тільки головний index.scss, бо в ньому вже є @import всіх інших
 function styles() {
   return src('src/scss/index.scss') 
-    .pipe(sass().on('error', sass.logError)) // Компілюємо SCSS -> CSS
-    .pipe(cssnano()) // Мінімізуємо (стискаємо) код
-    .pipe(rename('index.min.css')) // Перейменовуємо результат на index.min.css (згідно завдання)
-    .pipe(dest('dist/css')) // Кладемо в папку dist
-    .pipe(browserSync.stream()); // Оновлюємо браузер
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(rename('index.min.css')) // Як у завданні
+    .pipe(dest('dist/css'))
+    .pipe(browserSync.stream());
 }
 
-// JS
+// JS: переносить твої скрипти
 function scripts() {
   return src('src/js/**/*.js')
     .pipe(dest('dist/js'))
     .pipe(browserSync.stream());
 }
 
-// Images
+// Images: стискає картинки
 function images() {
   return src('src/images/**/*')
     .pipe(imagemin())
@@ -39,43 +42,51 @@ function images() {
     .pipe(browserSync.stream());
 }
 
-// Reload
-function reload(cb) {
-  browserSync.reload();
-  cb();
-}
-
-// Copy Bootstrap
+// Копіювання CSS Bootstrap
 function copyCSS() {
   return src('node_modules/bootstrap/dist/css/bootstrap.min.css')
     .pipe(dest('dist/css'));
 }
 
+// Копіювання JS Bootstrap
 function copyJS() {
   return src('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
     .pipe(dest('dist/js'));
 }
 
-// Server
+// Server: запускає локальний сервер і слідкує за змінами
 function serve() {
   browserSync.init({
     server: { baseDir: 'dist' },
-    port: 3000
+    port: 3000,
+    notify: false // Вимикає набридливе повідомлення справа зверху
   });
 
-  watch('src/**/*.html', html);
-  watch('src/scss/**/*.scss', styles);
+  // Якщо змінюється будь-який HTML (головний або компоненти) -> перезапуск html()
+  watch(['src/*.html', 'src/components/**/*.html'], html);
+  
+  // Якщо змінюється будь-який SCSS (головний, утиліти або компоненти) -> перезапуск styles()
+  watch(['src/scss/**/*.scss', 'src/components/**/*.scss'], styles);
+  
   watch('src/js/**/*.js', series(scripts, reload));
   watch('src/images/**/*', series(images, reload));
 }
 
-// Exports
+// Функція перезавантаження для JS/Images
+function reload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+// Експорт завдань
 exports.html = html;
 exports.scss = styles;
 exports.js = scripts;
 exports.images = images;
 exports.copyCSS = copyCSS;
 exports.copyJS = copyJS;
+
+// Дефолтний таск (запускається просто командою 'gulp')
 exports.default = series(
   parallel(html, styles, scripts, images, copyCSS, copyJS),
   serve
